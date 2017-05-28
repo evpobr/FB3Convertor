8c14437bf8874a44635a308252a03a46b3336c3f
evpobr <evpobr@users.noreply.github.com>
2017-05-28 17:16:41 +0500

    Create initial GUI

---
+++ FB3Convertor/MainForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace FB3Convertor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInputFile = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.buttonInputOpen = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOutputSave = new System.Windows.Forms.Button();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelInputFile
+            // 
+            this.labelInputFile.AutoSize = true;
+            this.labelInputFile.Location = new System.Drawing.Point(12, 9);
+            this.labelInputFile.Name = "labelInputFile";
+            this.labelInputFile.Size = new System.Drawing.Size(104, 13);
+            this.labelInputFile.TabIndex = 0;
+            this.labelInputFile.Text = "Select input FB2 file:";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxInput.Location = new System.Drawing.Point(12, 25);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(537, 20);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // buttonInputOpen
+            // 
+            this.buttonInputOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonInputOpen.Location = new System.Drawing.Point(555, 23);
+            this.buttonInputOpen.Name = "buttonInputOpen";
+            this.buttonInputOpen.Size = new System.Drawing.Size(106, 23);
+            this.buttonInputOpen.TabIndex = 2;
+            this.buttonInputOpen.Text = "Open...";
+            this.buttonInputOpen.UseVisualStyleBackColor = true;
+            this.buttonInputOpen.Click += new System.EventHandler(this.buttonInputOpen_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "fb2";
+            this.openFileDialog.FileName = "*.fb2";
+            this.openFileDialog.Filter = "FictionBook 2.0 files|*.fb2|All files|*.*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select output FB3 filename:";
+            // 
+            // buttonOutputSave
+            // 
+            this.buttonOutputSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOutputSave.Location = new System.Drawing.Point(555, 82);
+            this.buttonOutputSave.Name = "buttonOutputSave";
+            this.buttonOutputSave.Size = new System.Drawing.Size(106, 23);
+            this.buttonOutputSave.TabIndex = 5;
+            this.buttonOutputSave.Text = "Save...";
+            this.buttonOutputSave.UseVisualStyleBackColor = true;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxOutput.Location = new System.Drawing.Point(12, 84);
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(537, 20);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonConvert.Location = new System.Drawing.Point(555, 123);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(106, 23);
+            this.buttonConvert.TabIndex = 6;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 311);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.buttonOutputSave);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonInputOpen);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelInputFile);
+            this.Name = "MainForm";
+            this.Text = "FB3Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInputFile;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button buttonInputOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOutputSave;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonConvert;
+    }
+}
+
--- FB3Convertor/Program.cs
+++ FB3Convertor/Program.cs
@@ -16,7 +16,7 @@ namespace FB3Convertor
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
